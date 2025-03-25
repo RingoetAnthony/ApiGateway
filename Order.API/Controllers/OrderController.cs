287aaa366cf240d2237b5283b46bfdbb50435bb0
netlab111 <netlab111@tftic.be>
2025-03-25 12:36:35 +0100

    first commit

---
+++ Order.API/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(new[] {"command 1", "command 2", "command 3"});
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Your command is : {id}");
+        }
+
+        [HttpGet("post/{id}")]
+        public IActionResult Post(int id)
+        {
+            return Ok ($"Your Order {id} was confirmed");
+        }
+
+        [HttpGet("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Your Order {id} was deleted");
+        }
+
+        [HttpGet("edit/{id}")]
+        public IActionResult Put(int id)
+        {
+            return Ok($"Your Order {id} was modified");
+        }
+    }
+}
---
+++ Product.API/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Product.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(new[] {"product 1", "product 2", "product 3"});
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Your product number : {id}");
+        }
+    }
+}
